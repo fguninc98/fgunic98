d96301cc60c37177abf91eb96b21f981ebeb236b
Cofi <Cofi@Cofi-PC>
2019-12-17 20:43:05 +0100

    Initial commit

---
+++ rest-api/src/test/java/SK2Microservices/restapi/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package SK2Microservices.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
