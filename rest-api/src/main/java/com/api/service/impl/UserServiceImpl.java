61df43971a27a770220472cadb1a82685458a08c
Aleksandar Stojanovic <astojanovic17@raf.rs>
2019-12-28 14:38:06 +0100

    Fixed mapper

--- rest-api/src/main/java/com/api/service/impl/UserServiceImpl.java
+++ rest-api/src/main/java/com/api/service/impl/UserServiceImpl.java
@@ -10,8 +10,8 @@ import com.api.dto.TokenResponseDto;
 import com.api.dto.UserCreateDto;
 import com.api.dto.UserDto;
 import com.api.exeptions.NotFoundException;
-import com.api.mapper.UserMapper;
 import com.api.model.User;
+import com.api.repository.RoleRepository;
 import com.api.repository.UserRepository;
 import com.api.service.UserService;
 import com.api.tokenService.TokenService;
@@ -24,25 +24,29 @@ public class UserServiceImpl implements UserService {
 
 	private TokenService tokenService;
 	private UserRepository userRepository;
+	private RoleRepository roleRepository;
 	private ModelMapper userMapper;
 	
 	
-	public UserServiceImpl(TokenService tokenService, UserRepository userRepository) {
+	public UserServiceImpl(TokenService tokenService, UserRepository userRepository, RoleRepository roleRepository) {
 		super();
 		this.tokenService = tokenService;
 		this.userRepository = userRepository;
+		this.roleRepository = roleRepository;
 		this.userMapper = new ModelMapper();
 	}
 
 	@Override
 	public Page<UserDto> findAll(Pageable pageeable) {
-		return userRepository.findAll(pageeable).map(userMapper::map);
+		//return (Page<UserDto>) userMapper.map(userRepository.findAll(pageeable), UserDto.class);
+		return null;
 	}
 
 	@Override
 	public UserDto add(UserCreateDto userCreateDto) {
 		//User user = userMapper.userCreateDtoToUser(userCreateDto);
 		User user = userMapper.map(userCreateDto, User.class);
+		user.setRole(roleRepository.findByName("ROLE_USER").get());
 		userRepository.save(user);
 		//return userMapper.userToUserDto(user);
 		return userMapper.map(user, UserDto.class);
@@ -57,5 +61,4 @@ public class UserServiceImpl implements UserService {
 		claims.put("role",user.getRole().getName());
 		return new TokenResponseDto(tokenService.generate(claims));
 	}
-
 }
