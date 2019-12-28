d8ed8830f7660a70c9fb2f72da21edd41cd7ce45
Cofi <Cofi@Cofi-PC>
2019-12-29 00:55:51 +0100

    Prepravljen user controller,treba prepraviti user mapper pa testirati

--- rest-api/src/main/java/com/api/controller/AuthController.java
+++ rest-api/src/main/java/com/api/controller/AuthController.java
@@ -2,26 +2,44 @@ package com.api.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.model.User;
 import com.api.repository.UserRepository;
+import com.api.security.CheckSecurity;
+import com.api.service.UserService;
+
+
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+
+import com.api.dto.TokenRequestDto;
+import com.api.dto.TokenResponseDto;
+import com.api.dto.UserCreateDto;
+import com.api.dto.UserDto;
+
 
 @RestController
 public class AuthController {
-	private UserRepository userRepository;
+	private UserService userService;
 
-	public AuthController(UserRepository userRepository) {
+	public AuthController(UserService userService) {
 		super();
-		this.userRepository = userRepository;
+		this.userService = userService;
 	}
 
+	/*
 	@RequestMapping("/test")
 	public User greeting(@RequestParam(value="name", defaultValue="World") String name) {
 		User test = new User();
@@ -34,5 +52,33 @@ public class AuthController {
 		userRepository.save(user);
 		return new ResponseEntity<Object>(HttpStatus.OK);
 	}
-
+	 */
+	
+	
+    @ApiOperation(value = "Get all users")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page", value = "What page number you want", dataType = "string", paramType = "query"),
+            @ApiImplicitParam(name = "size", value = "Number of items to return", dataType = "string", paramType = "query"),
+            @ApiImplicitParam(name = "sort", allowMultiple = true, dataType = "string", paramType = "query",
+                    value = "Sorting criteria in the format: property(,asc|desc). " +
+                            "Default sort order is ascending. " +
+                            "Multiple sort criteria are supported.")})
+    @GetMapping
+    @CheckSecurity(roles = {"ROLE_ADMIN", "ROLE_USER"})
+	public ResponseEntity<Page<UserDto>> getAllUsers(@RequestHeader("Authorization") String authorization,Pageable pageable){
+		return new ResponseEntity<>(userService.findAll(pageable),HttpStatus.OK);
+	}
+    
+    @ApiOperation(value = "Register user")
+    @PostMapping
+    public ResponseEntity<UserDto> saveUser(@RequestBody @Valid UserCreateDto userCreateDto) {
+        return new ResponseEntity<>(userService.add(userCreateDto), HttpStatus.CREATED);
+    }
+	
+    @ApiOperation(value = "Login")
+    @PostMapping("/login")
+    public ResponseEntity<TokenResponseDto> loginUser(@RequestBody @Valid TokenRequestDto tokenRequestDto) {
+        return new ResponseEntity<>(userService.login(tokenRequestDto), HttpStatus.OK);
+    }
+	
 }
